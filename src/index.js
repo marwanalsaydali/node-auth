8becc922bcc8e14cd2a8f57b00d75a5ce6e4a699
Marwan Alsaydali <hey@marwanalsaydali.com>
2021-07-15 09:20:37 -0400

    setuping a basic server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-import "./db.js";
+import { fastify } from "fastify";
+// import the support for static html
+import fastifyStatic from "fastify-static";
+// we doing below becuase we don't have domain yet
+import path from "path";
+import { fileURLToPath } from "url";
+// ESM soecific feature
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-console.log("hello from index");
+const app = fastify();
+
+async function startApp() {
+  try {
+    // get the html
+    app.register(fastifyStatic, {
+      root: path.join(__dirname, "../public"),
+    });
+    // testing the return but not goint to use👇🏻
+    // app.get("/", {}, (request, reply) => {
+    //   reply.send({
+    //     data: "hello Worldz",
+    //   });
+    // });
+    // listen to the port
+    await app.listen(3002);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+startApp();
